bdf5c68343d436d3994512914a31cc43cc63b3e9
amirxl <91869579+amirxl@users.noreply.github.com>
2021-10-14 22:41:07 +0330

    Add files via upload

---
+++ for.py
@@ -0,0 +1,13 @@
+#mesal sade for
+
+for x in range(2, 9):
+  print(x)
+
+"""-----------------------------"""
+
+#mesal for va if
+for x in range(10):
+  if x == 5: break
+  print(x)
+else:
+  print("Finally finished!")
