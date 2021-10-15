bfc2002d923b36e5a994a4355b245c11b49760de
amirxl <91869579+amirxl@users.noreply.github.com>
2021-10-15 10:46:17 +0330

    Create dict.py

---
+++ syntax/dict.py
@@ -0,0 +1,13 @@
+#dictionary mesal
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+print(len(thisdict))
+#len toole dict  ro mide
+print(thisdict.keys())
+#keys kelid haro neshon mide
+print(thisdict.values())
+#va value meqdar dovvom va arzeshe key ro neshon 
