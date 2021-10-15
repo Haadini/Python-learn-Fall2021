7d26b111eb7600dca8194247f10829555bebb874
amirxl <91869579+amirxl@users.noreply.github.com>
2021-10-15 10:02:46 +0330

    Add files via upload

---
+++ prelem-amirbahjat/nasted.py
@@ -0,0 +1,29 @@
+#If to dar to yek b yek check kone
+#mesal avval to dar to
+
+num=9
+if num==5:
+    print("number =5")
+else:
+    if num==6:
+        print("number =6")
+    else:
+        if num==7:
+            print("number =7")
+        else:
+            print("number don't equal 5,6,7")
+"""----------------------------------------------------------------"""
+#While to dar to inno barash fekr konin
+#mesal dovom to dar to
+i=0
+while i<10:
+    print (i)
+    i+=1
+    j=5
+    while j<i:
+        print (j)
+        j+=1
+        h=15
+        while h>j:
+            print (h)
+            h-=2
